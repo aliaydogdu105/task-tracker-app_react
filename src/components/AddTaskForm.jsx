fc9e64738b628e70e24b570a631e686ac24259c6
aliaydogdu105 <aliaydogdu105@gmail.com>
2022-07-26 18:57:59 +0300

    addtask

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -1,4 +1,45 @@
-const AddTaskForm = () => {
-  return <div>show-close-form</div>;
+import { useState } from "react";
+
+const AddTaskForm = ({ tasks, setTasks }) => {
+  const [task, setTask] = useState("");
+  const [day, setDay] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = new Date().getTime();
+    const newTask = { id: id, task: task, day: day, isDone: false };
+    setTasks([...tasks, newTask]);
+    setTask("");
+    setDay("");
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="task">Task</label>
+          <input
+            type="text"
+            name="task"
+            id="task"
+            placeholder="add a task"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="day">Day</label>
+          <input
+            type="date"
+            name="day"
+            id="day"
+            value={day}
+            onChange={(e) => setDay(e.target.value)}
+          />
+        </div>
+        <div>
+          <button className="btn btn-submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 export default AddTaskForm;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import Header from "../components/Header";
 import ShowTasks from "../components/ShowTasks";
+import data from "../helper/starterData";
 
 const Home = () => {
+  const [tasks, setTasks] = useState(data);
+
   return (
     <div>
-      <Header />
+      <Header tasks={tasks} setTasks={setTasks} />
       <ShowTasks />
     </div>
   );
