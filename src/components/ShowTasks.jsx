c87c7b6f47011bf900f0481199ded9f9e3e6fdf7
aliaydogdu105 <aliaydogdu105@gmail.com>
2022-07-27 17:06:40 +0300

    showtasks

--- src/components/ShowTasks.jsx
+++ src/components/ShowTasks.jsx
@@ -1,5 +1,39 @@
-const ShowTasks = () => {
-  return <div>show tasks</div>;
+import React, { useState } from "react";
+
+const ShowTasks = ({ tasks, setTasks }) => {
+  const [done, setDone] = useState(true);
+
+  const handleDelete = (id) => {
+    setTasks(tasks.filter((item) => item.id !== id));
+  };
+
+  const handleDone = (e) => {
+    if (done) {
+      e.target.classList.add("done");
+      setDone(!done);
+    } else {
+      e.target.classList.remove("done");
+      setDone(!done);
+    }
+  };
+
+  return tasks.map((item) => {
+    const { id, task, day } = item;
+
+    return (
+      <div>
+        <div className="" key={id} onClick={handleDone}>
+          <div className="">
+            <h2>{task}</h2>
+            <p>{day} </p>
+          </div>
+          <div className="" onClick={() => handleDelete(id)}>
+            delete
+          </div>
+        </div>
+      </div>
+    );
+  });
 };
 
 export default ShowTasks;
